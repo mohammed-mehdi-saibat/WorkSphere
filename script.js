b6489e22f5b1b3fc6d8e3be7bb0a29c31f7a7895
mohammed mehdi <mohammedmehdisaibat@gmail.com>
2025-11-18 15:01:50 +0100

    build base UI HTML/CSS/JS -- still in progress

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const addWorker = document.querySelector(".add-worker");
+const addWorkerPopup = document.querySelector(".add-worker-popup");
+const closeBtn = document.querySelector(".close");
+const sideBar = document.querySelector(".side-bar");
+const worksphere = document.querySelector(".worksphere");
+
+addWorker.addEventListener("click", () => {
+  addWorkerPopup.showModal();
+  sideBar.style.filter = "blur(2.2px)";
+  worksphere.style.filter = "blur(2.2px)";
+});
+
+closeBtn.addEventListener("click", () => {
+  addWorkerPopup.close();
+  sideBar.style.filter = "blur(0)";
+  worksphere.style.filter = "blur(0)";
+});
