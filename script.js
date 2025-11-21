fd65050b4c3f726ae8994a65c3d2a2d671defa9a
mohammed mehdi <mohammedmehdisaibat@gmail.com>
2025-11-21 10:01:55 +0100

    struggle to find add experience logic

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 "use strict";
 
-// --------------------SIDE BAR MATERIALS--------------
+// --------------------SIDE BAR MATERIAL--------------
+//----BUTTONS
 const closeBtn = document.querySelector("#cancel");
 const saveBtn = document.querySelector("#save");
-const sideBar = document.querySelector(".side-bar");
-const worksphere = document.querySelector(".worksphere");
+const deleteBtn = document.getElementById("delete");
 const addWorker = document.querySelector(".add-worker");
-const addWorkerPopup = document.querySelector(".add-worker-popup");
-const workersList = document.getElementById("workers-list");
+const addExperience = document.getElementById("add-experience");
+const cancelExperience = document.querySelector(".cancel-experience");
+//----BUTTONS
+//======================================================
+//----INPUTS
+const addWorkerPopupForm = document.getElementById("add-worker-form");
 const workerFirstName = document.getElementById("nom");
+const workerPhoneNumber = document.getElementById("phone");
 const workerLastName = document.getElementById("prenom");
 const workerEmail = document.getElementById("email");
-const workerPhoneNumber = document.getElementById("phone");
+const workerProfilePictureUrl = document.getElementById("profile");
+const workerExperienceStart = document.getElementById("from");
+const workerExperienceEnd = document.getElementById("to");
+const workerExperienceRole = document.getElementById("experience-role");
 const selectOptions = document.getElementById("roles");
-const deleteBtn = document.getElementById("delete");
-// --------------------SIDE BAR MATERIALS--------------
+const workerExperience = document.getElementById("experience");
+const error = document.querySelector(".error-message");
+//----INPUTS
+//======================================================
+//----MAIN CONTAINERS
+const sideBar = document.querySelector(".side-bar");
+const worksphere = document.querySelector(".worksphere");
+const workersList = document.getElementById("workers-list");
+const addWorkerPopup = document.querySelector(".add-worker-popup");
+const experiences = document.querySelector(".experiences");
+//----MAIN CONTAINERS
+const workerProfilePicture = document.querySelector(".profile-picture");
+//======================================================
+// --------------------SIDE BAR MATERIAL--------------7
+//======================================================
+//--------------------- REGEX CHECK-----------------
+const workerFirstNameRegex = /^[a-zA-Z\s].{3,}$/;
+const workerLastNameRegex = /^[a-zA-Z\s].{3,}$/;
+const workerExperienceRoleRegex = /^[a-zA-Z\s].{3,}$/;
+const workerExperienceRegex = /^[^\s][a-zA-Z0-9\s].{3,}$/;
+const workerEmailRegex = /^[^\s@]+@[^\s@]+.[^\s@]$/;
+const workerPhoneRegex = /^0[6-7]\s?\d{2}\s?\d{2}\s?\d{2}\s?\d{2}$/;
 
+//--------------------- REGEX CHECK-----------------
+//======================================================
 // -----------------------ROLES SELECTION-------------------
 const roles = [
   "Réceptionnistes",
@@ -24,6 +54,7 @@ const roles = [
   "Manager",
   "Nettoyage",
 ];
+
 function rolesFill() {
   roles.forEach((role) => {
     const option = document.createElement("option");
@@ -33,10 +64,8 @@ function rolesFill() {
 }
 rolesFill();
 // -----------------------ROLES SELECTION-------------------
-
+//======================================================
 // -------------------------STAFF MANIPULATION----------------
-const workers = [];
-
 function addNewWorker() {
   addWorker.addEventListener("click", () => {
     showAddWorkerModal();
@@ -47,14 +76,20 @@ function addNewWorker() {
   saveBtn.addEventListener("click", () => {
     addWorkerData();
   });
+  addExperience.addEventListener("click", (e) => {
+    e.preventDefault();
+    addNewExperience();
+    cancelExperience.classList.toggle("showCancelButton");
+    // console.log("Hey there I am the cancel button");
+  });
 }
 
 addNewWorker();
 
 function showAddWorkerModal() {
   addWorkerPopup.showModal();
-  sideBar.style.filter = "blur(2.2px)";
-  worksphere.style.filter = "blur(2.2px)";
+  sideBar.style.filter = "blur(2.5px)";
+  worksphere.style.filter = "blur(2.5px)";
 }
 
 function hideAddWorkerModal() {
@@ -63,16 +98,38 @@ function hideAddWorkerModal() {
   worksphere.style.filter = "blur(0)";
 }
 
+addWorkerPopup.addEventListener("click", (e) => {
+  if (e.target === addWorkerPopup) {
+    addWorkerPopup.close();
+    sideBar.style.filter = "blur(0)";
+    worksphere.style.filter = "blur(0)";
+  }
+});
+
+const workers = [];
+
 function addWorkerData() {
   const worker = {
     name: workerFirstName.value,
+    lastName: workerLastName,
+    email: workerEmail.value,
+    phone: workerPhoneNumber.value,
     role: selectOptions.value,
-    profile: "p",
+    profile: workerProfilePictureUrl.value,
+    experiences: [
+      {
+        experience: workerExperience.value,
+        experienceRole: workerExperienceRole.value,
+        from: workerExperienceStart.value,
+        to: workerExperienceEnd.value,
+      },
+    ],
   };
+
   const newWorker = document.createElement("div");
   newWorker.classList.add("worker");
   newWorker.innerHTML = `
-    <div class="profile-picture">${worker.profile}</div>
+    <div id="profile-picture" class="profile-picture"></div>
     <div class="worker-info">
     <h5 class="name">${worker.name}</h5>
     <small style="color: red; font-size: 12px" class="role"
@@ -92,10 +149,55 @@ function addWorkerData() {
     id="delete"
     />
     `;
-  workers.push(worker);
+  workerProfilePicture.style.background = `url("${worker.profile}") center/cover no-repeat`;
+
   workersList.appendChild(newWorker);
+  workers.push(worker);
+  console.log(workers);
   hideAddWorkerModal();
 }
-console.log(workers);
 
+let newExperiences = [];
+
+function addNewExperience() {
+  const newExperience = document.createElement("div");
+
+  newExperience.innerHTML = `
+            <div class="experiences-separator"></div>
+            <input id="experience" type="text" placeholder="Company:" />
+            <small class="error-message"></small>
+            <input id="experience-role" type="text" placeholder="role:" />
+            <small class="error-message"></small>
+            <div class="experience-dates">
+              <label for="from">From:</label>
+              <input
+                type="date"
+                name=""
+                id="from"
+                placeholder="mm/dd/yyyy"
+                onfocus="this.type='date'"
+                onblur="if(this.value === '') this.type='text'"
+              />
+              <label for="to">To</label>
+              <input type="date" / name="" id="to" placeholder="mm/dd/yyyy"
+              onfocus="this.type='date'" onblur="if(this.value === '')
+              this.type='text'">
+            </div>
+  `;
+
+  newExperiences.push(newExperience);
+  console.log("=======================");
+  console.log("NEW EXPERIENCE WAS ADDED");
+  console.log(newExperiences);
+  console.log(`LENGTH: ${newExperiences.length}`);
+  console.log("=======================");
+
+  // experiences.appendChild(newExperience);
+  // console.log("Yo I'm working!");
+  // cancelExperience.addEventListener("click", (e) => {
+  //   e.preventDefault();
+  //   experiences.removeChild(newExperience);
+  // });
+}
 // -------------------------STAFF MANIPULATION----------------
+//======================================================
